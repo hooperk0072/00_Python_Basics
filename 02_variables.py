8e8d914455f8dafd10f9bd470ee0bf068268f0f9
hooperk0072 <85589688+hooperk0072@users.noreply.github.com>
2021-06-09 10:52:25 +1200

    uhh idk

---
+++ 02_variables.py
@@ -0,0 +1,13 @@
+# code to learn about variables
+
+#variables can also be a string
+greeting = 'hello world'
+
+#they can also be numbers
+fav_num = 7
+print(fav_num * 2)
+#this variable has a problem
+
+
+
+
---
+++ 03_getting_input.py
@@ -0,0 +1,7 @@
+username = input("What is your name? ")
+fav_num = int(input("Favourite Number? "))
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print("Hi, {}, your favourite number is {}. {} doubled is {}, halved is {}, and squared is {}.".format(username, fav_num, fav_num, double, half, squared))
